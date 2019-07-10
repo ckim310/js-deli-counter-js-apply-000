db0dec7df81b8d960d04d502500687b99e909b20
Christine K <ckim31093@gmail.com>
2019-07-10 03:39:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(arr, firstName) {
   arr.push(firstName);
-  return `Welcome, ${firstName}. You are number ${arr.length + 1} in line.`;
+  return `Welcome, ${firstName}. You are number ${arr.length} in line.`;
 }
